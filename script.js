debbd46653de295fe6906074896e5e77576d99eb
Houze <ljhao177@outlook.com>
2023-07-12 11:56:14 +0800

    add plusButton func

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ document.addEventListener('keypress', (event) => {
     const keyName = event.key;
     console.log('countNum = ' + keyName);
     countNum++;
-
+    document.getElementById("count").innerHTML = countNum;
 }, false);
 
 function resetButton() {
     countNum = 0;
+    document.getElementById("count").innerHTML = countNum;
+}
+
+function plusButton() {
+    countNum++
     document.getElementById("count").innerHTML = countNum;
 }
